3cec1b2f8a0170513387aab0ccb653ad8c6fe7aa
Marcos V. Pretti <103339147+eppou@users.noreply.github.com>
2023-09-07 20:50:36 -0300

    Delete Web/RestauranteRepository.java

--- Web/RestauranteRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.Prova1ConsumidorMarcosDias;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RestauranteRepository extends CrudRepository<Restaurante,Integer>{}
